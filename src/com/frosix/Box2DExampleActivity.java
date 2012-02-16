9feeb82a25ee0cb04a19b6ccaff46bdb143ef327
ratrak7@gmail.com <ratrak7@gmail.com@060dce88-f7ff-3c98-31c1-3dbf993959cb>
2012-02-16 12:08:21 +0000

    Box2d updated

--- src/com/frosix/Box2DExampleActivity.java
+++ src/com/frosix/Box2DExampleActivity.java
@@ -7,6 +7,9 @@ import java.util.Map;
 
 import org.anddev.andengine.engine.Engine;
 import org.anddev.andengine.engine.camera.Camera;
+import org.anddev.andengine.engine.handler.IUpdateHandler;
+import org.anddev.andengine.engine.handler.timer.ITimerCallback;
+import org.anddev.andengine.engine.handler.timer.TimerHandler;
 import org.anddev.andengine.engine.options.EngineOptions;
 import org.anddev.andengine.engine.options.EngineOptions.ScreenOrientation;
 import org.anddev.andengine.engine.options.resolutionpolicy.RatioResolutionPolicy;
@@ -18,6 +21,7 @@ import org.anddev.andengine.entity.scene.Scene.ITouchArea;
 import org.anddev.andengine.entity.scene.background.ColorBackground;
 import org.anddev.andengine.entity.shape.Shape;
 import org.anddev.andengine.entity.sprite.AnimatedSprite;
+import org.anddev.andengine.entity.sprite.Sprite;
 import org.anddev.andengine.extension.physics.box2d.PhysicsConnector;
 import org.anddev.andengine.extension.physics.box2d.PhysicsFactory;
 import org.anddev.andengine.extension.physics.box2d.PhysicsWorld;
@@ -30,14 +34,18 @@ import org.anddev.andengine.ui.activity.BaseGameActivity;
 
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.Contact;
+import com.badlogic.gdx.physics.box2d.ContactImpulse;
+import com.badlogic.gdx.physics.box2d.ContactListener;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
+import com.badlogic.gdx.physics.box2d.Manifold;
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 
 import android.hardware.SensorManager;
 import android.util.Log;
 import android.view.Display;
 
-public class Box2DExampleActivity extends BaseGameActivity  {
+public class Box2DExampleActivity extends BaseGameActivity implements IOnSceneTouchListener  {
 
 	private static int CAMERA_HEIGHT;
 	private static float CAMERA_WIDTH ;
@@ -47,10 +55,17 @@ public class Box2DExampleActivity extends BaseGameActivity  {
 	private Scene mScene;
 	private PhysicsWorld mPhysicsWorld;
 	private Map<Integer ,Body> bodyMap = new HashMap<Integer,Body>(); 
-	private float[] startPoint =  new float[2];
-	private float[] endPoint =  new float[2];
+
+	
+
+	private Body rectBody ;
+	private Body globBody ;
+	
+	Rectangle rect;
+	private boolean moveFlag =false;
+	private boolean isRight =false;
 	//Const 
-	private static final FixtureDef FIXTURE_DEF = PhysicsFactory.createFixtureDef(1, 0.5f, 0.5f);
+	private static final FixtureDef FIXTURE_DEF = PhysicsFactory.createFixtureDef(1, 0.7f, 0.5f);
 	//
 	
 	@Override
@@ -86,12 +101,15 @@ public class Box2DExampleActivity extends BaseGameActivity  {
 		//the gravity vector is passed to constructor
 		this.mPhysicsWorld = new PhysicsWorld(new Vector2(0, 0), false);
 		
+		
 		final Shape ground = new Rectangle(0, CAMERA_HEIGHT - 2, CAMERA_WIDTH, 2);
 		final Shape roof = new Rectangle(0, 0, CAMERA_WIDTH, 2);
-		final Shape left = new Rectangle(0, 0, 2, CAMERA_HEIGHT);
+	//	final Shape left = new Rectangle(0, 0, 2, CAMERA_HEIGHT);
+		final Shape left = new Rectangle(- CAMERA_HEIGHT/2, CAMERA_HEIGHT/2, CAMERA_HEIGHT, 2);
+		left.setRotation(90);
 		final Shape right = new Rectangle(CAMERA_WIDTH - 2, 0, 2, CAMERA_HEIGHT);
-
-		final FixtureDef wallFixtureDef = PhysicsFactory.createFixtureDef(0, 0.5f, 0.5f);
+		
+		final FixtureDef wallFixtureDef = PhysicsFactory.createFixtureDef(0, 0.3f, 0.5f);
 		PhysicsFactory.createBoxBody(this.mPhysicsWorld, ground, BodyType.StaticBody, wallFixtureDef);
 		PhysicsFactory.createBoxBody(this.mPhysicsWorld, roof, BodyType.StaticBody, wallFixtureDef);
 		PhysicsFactory.createBoxBody(this.mPhysicsWorld, left, BodyType.StaticBody, wallFixtureDef);
@@ -107,31 +125,17 @@ public class Box2DExampleActivity extends BaseGameActivity  {
 		
 		mScene.setTouchAreaBindingEnabled(true);
 		
-		mScene.setOnAreaTouchListener(new IOnAreaTouchListener() {
-
+		
+		mScene.registerUpdateHandler(new TimerHandler (2, false ,  new ITimerCallback() {
+			
 			@Override
-			public boolean onAreaTouched(TouchEvent pSceneTouchEvent,
-					ITouchArea pTouchArea, float pTouchAreaLocalX,
-					float pTouchAreaLocalY) {
-				if(pSceneTouchEvent.isActionDown()){
-					startPoint[0] = pSceneTouchEvent.getX();
-					startPoint[1] = pSceneTouchEvent.getY();
-				}
-				else if(pSceneTouchEvent.isActionUp()){
-					endPoint[0] = pSceneTouchEvent.getX();
-					endPoint[1] = pSceneTouchEvent.getY();
-					
-					AnimatedSprite sprite = (AnimatedSprite) pTouchArea;
-					Body mBody = bodyMap.get(sprite.getUserData());
-					makeEffect(mBody, new Vector2(endPoint[0] - startPoint[0] , endPoint[1] - startPoint[1]));
-				}
-					
-					
+			public void onTimePassed(TimerHandler pTimerHandler) {
 				
-				return true;
-			}});
-
-				
+				makeEffect(globBody, new Vector2(0, 20));
+			}
+		}));
+		
+		mScene.setOnSceneTouchListener(this);	
 				
 		return mScene;
 	}
@@ -139,21 +143,70 @@ public class Box2DExampleActivity extends BaseGameActivity  {
 	
 	@Override
 	public void onLoadComplete() {	
-		addFace(1);
-		addFace(2);
-		addFace(3);
+		globBody = addFace(1);
+		
+		addControl();
+		mScene.registerUpdateHandler(new IUpdateHandler() {
+			
+			@Override
+			public void reset() {						
+			}
+			
+			@Override
+			public void onUpdate(float pSecondsElapsed) {
+				movePlatform(moveFlag ,isRight )		;		
+			}
+		});
+		
+		mPhysicsWorld.setContactListener(new ContactListener() {
+			
+			@Override
+			public void preSolve(Contact contact, Manifold oldManifold) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void postSolve(Contact contact, ContactImpulse impulse) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void endContact(Contact contact) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			@Override
+			public void beginContact(Contact contact) {
+				Body ball ;
+				Body otherBody ;
+				
+				ball = contact.getFixtureA().getBody().getType().equals(BodyType.DynamicBody) ? contact.getFixtureA().getBody() : contact.getFixtureB().getBody();
+				otherBody =  ball==contact.getFixtureA().getBody() ? contact.getFixtureB().getBody() : contact.getFixtureA().getBody();
+			}
+		});
 		
 	}
-	public void makeEffect(Body pBody , Vector2 pVector ){
-		pBody.setLinearVelocity(pVector.mul(0.3f));
-		//pBody.
-	};
 	
-	private void addFace( int count){
+	public void makeEffect(Body pBody , Vector2 pVector ){
+		pBody.setLinearVelocity(pVector);
+		writeVectorToLog(pVector ," make effect");
+	    };
+	
+    public void writeVectorToLog( Vector2 pVector , String pStr){
+    	Log.i("flag", pStr + "  pVector=" + pVector.x  +" y:" +pVector.y );
+    }
+    
+	private Body addFace( int count){
 		
 		final AnimatedSprite face = new AnimatedSprite(0, 0, this.mCircleFaceTextureRegion);
-		face.setScale(2f);
-		final Body body = PhysicsFactory.createCircleBody(this.mPhysicsWorld, face, BodyType.DynamicBody, FIXTURE_DEF);
+		face.setScale(1.5f);
+		final Body body = PhysicsFactory.createCircleBody(this.mPhysicsWorld, face, BodyType.DynamicBody, PhysicsFactory.createFixtureDef(1, 1f, 0.5f));
+		
+		//Physicworld coordinates smaller then screen coord.
+		body.setTransform(240 /32 , 400/ 32, 0);
 		face.setUserData(count);
 		
 		if(!bodyMap.containsValue(count)){
@@ -161,11 +214,65 @@ public class Box2DExampleActivity extends BaseGameActivity  {
 		}
 		
 		this.mScene.attachChild(face);
-		mScene.registerTouchArea(face);
 		this.mPhysicsWorld.registerPhysicsConnector(new PhysicsConnector(face, body, true, true));
+		return body;
 		
 		
 	}
+
+	public void addControl(){
+		float rectWidth = 150;
+		float rectHeight = 20;
+		
+		rect = new Rectangle(0, CAMERA_HEIGHT - rectHeight , rectWidth, rectHeight);
+		rectBody = PhysicsFactory.createBoxBody(this.mPhysicsWorld, rect, BodyType.KinematicBody, PhysicsFactory.createFixtureDef(1, 1.2f, 0.5f));
+		rectBody.setTransform(0, (CAMERA_HEIGHT - rectHeight) /32, 0);
+		rect.setColor(255, 255, 255);
+		
+		this.mScene.attachChild(rect);
+		mScene.registerTouchArea(rect);
+		this.mPhysicsWorld.registerPhysicsConnector(new PhysicsConnector(rect, rectBody, true, true));
+	}
+	
+	@Override
+	public boolean onSceneTouchEvent(Scene pScene, TouchEvent pSceneTouchEvent) {
+		if(!pSceneTouchEvent.isActionUp()){
+			moveFlag = true;
+			if(pSceneTouchEvent.getX() > CAMERA_WIDTH/2){
+				isRight = true;
+				if((rect.getX() + rect.getWidth()) > CAMERA_WIDTH){
+					moveFlag = false;
+				}
+			}
+			else if (rect.getX() > 0){
+				isRight = false;
+				}
+				else{
+					moveFlag = false;
+				}
+		}
+		else{
+			moveFlag = false;
+		}
+		return true;
+	}
+	
+
+	public void movePlatform( boolean pMoveFlag , boolean pIsRight){
+		if(pMoveFlag){
+			if(pIsRight){
+				rectBody.setLinearVelocity(10, 0);
+			}
+			else{
+				rectBody.setLinearVelocity(-10 , 0);
+			}
+		}
+		else{
+			rectBody.setLinearVelocity(0, 0);
+		}
+	}
+	
+	
 
 	
 
